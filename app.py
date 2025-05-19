e6c6c4fba6a5c4dcbc3702cb8c2f46861c8b6e66
RamonLGDaw <ramonlg.daw@gmail.com>
2025-05-19 21:23:28 +0200

    update port

--- app.py
+++ app.py
@@ -157,5 +157,8 @@ def visita():
     else:
         return jsonify({'error': f'La aplicación {web_name} no está registrada'}), 404
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Usa el puerto de la variable de entorno, si no está presente, usa el puerto 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
